1a1f80b31470132f2521e46eaa49afd77fc4275e
Steven Michalove <88119992+smichalove@users.noreply.github.com>
2021-10-05 00:36:47 -0700

    Update AVM60onviaIP.py

--- AVM60onviaIP.py
+++ AVM60onviaIP.py
@@ -6,7 +6,7 @@
 #Your input to the Mark Levinson Should connect to GPIO21 for + and a GRND
 #The control Signal from constant high or low should connect to a 12 V relay
 #the switch signals from the relay to toggle on/off should be connected to GPIO26 and GRND
-# Radpberry Pico connected to USB on TV
+# Raspberry Pico connected to USB on TV
 # Pico siglnalling pin connected on GPIO20 pico (GP16) of Pi
 '''
 https://learn.adafruit.com/getting-started-with-raspberry-pi-pico-circuitpython/circuitpython
